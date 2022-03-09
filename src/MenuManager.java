cc766269c044ea2b63a949095c6bfee4fcd2e389
jooms4300 <jooms4300@naver.com>
2022-03-09 20:50:00 +0900

    removerd hello.java and manumanger.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. ADD Students");
+		System.out.println("1. Delete Students");
+		System.out.println("1. Edit Students");
+		System.out.println("1. View Students");
+		System.out.println("1. Exit");
+
+	}
+
+}
