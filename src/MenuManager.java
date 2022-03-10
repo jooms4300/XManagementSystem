b6aa4d1a385f13be1a0baa00280427eb312f17cf
jooms4300 <jooms4300@naver.com>
2022-03-10 15:00:26 +0900

    Edited menumanager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,10 +2,10 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. ADD Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
+		System.out.println("1. Add fitnees ceter members ");
+		System.out.println("2. Delete fitnees ceter members");
+		System.out.println("3. Edit fitnees ceter members");
+		System.out.println("4. View fitnees ceter members");
 		System.out.println("5. Exit");
 
 	}
