8ace0266bb2855ae425137d37e98e91e69345ff4
jooms4300 <jooms4300@naver.com>
2022-03-25 15:12:43 +0900

    progect-menu-selection-method

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -5,7 +5,6 @@ public class MenuManager {
 	public static void main(String[] args) {
 		int num=0;
 		Scanner input = new Scanner(System.in);
-		
 		while(num!=6) {
 			System.out.println("1. Add fitness ceter members ");
 			System.out.println("2. Delete fitness ceter members");
@@ -16,49 +15,29 @@ public class MenuManager {
 			System.out.print("Select one number between 1-6: ");
 			num = input.nextInt();
 			
-			switch(num) {
-			case 1:
-				{
-				System.out.print("Members Id : ");
-				int membersId = input.nextInt();
-				
-				System.out.print("Members Name : ");
-				String membersName = input.next();
-				
-				System.out.print("Members Password : ");
-				int membersPassword = input.nextInt();
-				
-				System.out.print("Members Email : ");
-				String membersEmail = input.next();
-				
-				System.out.print("Members Phonenumber : ");
-				int membersPhonenumber = input.nextInt();
-				
-				break;
-				}
-			case 2:
-				{
-					
-				}
-			case 3:
-				{
-					
-				}
-			case 4:
-				{
-				System.out.print("Members Id : ");
-				int membersId2 = input.nextInt();
-				}
-			case 5:
-				{
-					
-				}
-			case 6:
-				{
-					
-				}
+			if(num == 1) {
+				addMembers();
 			}
 		}
 	}
-
+	public static void addMembers(){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Members Id : ");
+		int membersId = input.nextInt();
+		System.out.print("Members Name : ");
+		String membersName = input.next();
+		System.out.print("Members Email : ");
+		String membersEmail = input.next();
+		System.out.print("Members Phonenumber : ");
+		String membersPhonenumber = input.next();
+	}
+	public static void DeleteMembers(){
+		
+	}
+	public static void EditMembers(){
+		
+	}
+	public static void ViewMembers(){
+	
+	}
 }
