96f9779220dedf90fdf10a8c4e5bc863afb24be4
jooms4300 <jooms4300@naver.com>
2022-04-03 19:14:40 +0900

    added class(Member,MemberManager)

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,59 +3,34 @@ import java.util.Scanner;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		int num=0;
 		Scanner input = new Scanner(System.in);
-		while(num!=6) {
+		MemberManager membermanager = new MemberManager(input);
+		int num=0;
+		while(num!=5) {
 			System.out.println("1. Add fitness ceter members ");
 			System.out.println("2. Delete fitness ceter members");
 			System.out.println("3. Edit fitness ceter members");
 			System.out.println("4. View fitness ceter members");
-			System.out.println("5. Show a menu");
-			System.out.println("6. Exit");
-			System.out.print("Select one number between 1-6: ");
+			System.out.println("5. Exit");
+			System.out.print("Select one number between 1-5: ");
 			num = input.nextInt();
 			
 			if(num == 1) {
-				addMembers();
+				membermanager.addMembers();
 			}
 			else if(num == 2) {
-				DeleteMembers();
+				membermanager.DeleteMembers();
 			}
 			else if(num == 3) {
-				EditMembers();
+				membermanager.EditMembers();
 			}
 			else if(num == 4) {
-				ViewMembers();
+				membermanager.ViewMembers();
 			}
 			else {
 				continue;
 			}
 		}
 	}
-	public static void addMembers(){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Members Id : ");
-		int membersId = input.nextInt();
-		System.out.print("Members Name : ");
-		String membersName = input.next();
-		System.out.print("Members Email : ");
-		String membersEmail = input.next();
-		System.out.print("Members Phonenumber : ");
-		String membersPhonenumber = input.next();
-	}
- 	public static void DeleteMembers(){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Members Id : ");
-		int membersId = input.nextInt();
-	}
-	public static void EditMembers(){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Members Id : ");
-		int membersId = input.nextInt();
-	}
-	public static void ViewMembers(){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Members Id : ");
-		int membersId = input.nextInt();
-	}
+	
 }
